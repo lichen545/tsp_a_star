0a61f6e10b6f4892d9df20f0a6648fdfb34c8acd
CastleChester <lichen545@gmail.com>
2018-12-14 22:58:47 -0600

    added input generator script

---
+++ generate_problem.py
@@ -0,0 +1,18 @@
+import itertools
+import sys
+from random import randint
+
+MIN = 0
+MAX = 500
+
+def generate_input(n, filename):
+    with open(filename, "w") as f:
+        f.write("{n}\n".format(n=n))
+        for _ in itertools.repeat(None, n):
+            f.write("{p0} {p1}\n".format(p0=randint(MIN, MAX), p1=randint(MIN, MAX)))
+
+if __name__ == "__main__":
+    # Write the input files for the optimal solution first.
+    n = int(sys.argv[1])
+    filename = "tsp{}.txt".format(n)
+    generate_input(n, filename)
