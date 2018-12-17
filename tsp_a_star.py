ee4bb290ceb57edd4198ae3cf6103b33d968d7c7
CastleChester <lichen545@gmail.com>
2018-12-17 00:08:19 -0600

    added naive euclidean heuristic

--- tsp_a_star.py
+++ tsp_a_star.py
@@ -4,42 +4,30 @@ import timeit
 import math
 import heapq
 import itertools
+import pprint
 
 # data structure for reading in file and converting to a graph
 class Graph:
-    # source and sink are equivalent to startpt (1 in this example)
-    # node: [(neighbor, cost)]
-    # {   
-    #     ('source', {}): [ ((2, {2}), d(1,2)), ((3, {3}), d(1,3)), ((4, {4}), d(1,4)) ] 
-    #     (2, {2}): [ ((3, {2,3}), d(2,3)), ((4, {2,4}), d(2,4)) ],
-    #     ...,
-    #     ((3, {2,3}): [ ((4, {2,3,4}), d(3,4)) ],
-    #     ...,
-    #     (4, {2,3,4}): [ ("sink", d(4,1)) ],
-    # }
     ''' 
-    (x, S): starting from 1, path min cost ends at vertex x, passing vertices in set S exactly once
-    source and sink are equivalent to startpt
+    # source and sink are equivalent to start_point (1 in this example)
+    # (x, S): starting from 1, path min cost ends at vertex x, passing vertices in set S exactly once
     {   
-        ("source", None): [ (2, None), (3, None), (4, None) ] 
-        (2, None): [ (3, (2)), (4, (2)) ],
+        ("source", None): [ (2, ()), (3, ()), (4, ()) ] 
+        (2, ()): [ (3, (2)), (4, (2)) ],
         ...,
         (3, (2)): [ (4, (2,3)) ],
         ...,
         (4, (2,3)): [ ("sink", (2,3,4)) ],
     }
-
     '''
     # adjacency list for graph, takes a start point and a list of cities as input
-    def __init__(self, startpt, cities):
-        self.startpt = startpt
+    def __init__(self, start_point, cities):
+        self.start_point = start_point
+        self.cities = set(cities)
         self.edges = {}
         # add source node with key "source"
         cities = set(cities)
-        cities.discard(startpt)
-        # create all possible combinations of cities
-        # [[(2,), (3,), (4,)], [(2, 3), (2, 4), (3, 4)], [(2, 3, 4)]]
-        # city_combinations = [list(itertools.combinations(cities, n)) for n in range(1, len(cities) + 1)]
+        cities.discard(start_point)
         # create source node
         connected_values = [(c, ()) for c in cities]
         self.edges[('source', None)] = connected_values
@@ -62,19 +50,21 @@ class Graph:
                     self.edges[(x,S)] = [("sink", next_S)]
             connected_values = tmp
     
-    def __repr__(self):
+    def __str__(self):
         return str(self.__dict__)
 
-    def neighbors(self, point):
-        return self.edges[point]
+    # returns all neighbors of given node
+    def neighbors(self, node):
+        return self.edges[node]
 
+    # return cost of edge (from_node, to_node)
     def cost(self, from_node, to_node):
         from_point = from_node[0]
         to_point = to_node[0]
         if from_point == "source":
-            return rectilinear(self.startpt, to_point)
+            return rectilinear(self.start_point, to_point)
         elif to_point == "sink":
-            return rectilinear(from_point, self.startpt)
+            return rectilinear(from_point, self.start_point)
         else:
             return rectilinear(from_point, to_point)
 
@@ -102,9 +92,10 @@ def euclidean(a, b):
     (x_2, y_2) = b
     return math.sqrt(((x_1 - x_2) ** 2) + ((y_1 - y_2) ** 2))
 
-def held_karp(startpt, cities):
+# implementation of the held-karp algorithm for solving TSP
+def held_karp(start_point, cities):
     cities = set(cities)
-    cities.remove(startpt)
+    cities.remove(start_point)
     cities = tuple(cities)
 
     # Quick and dirty memoization
@@ -112,13 +103,13 @@ def held_karp(startpt, cities):
 
     def min_dist(loc, path):
         """
-        Minimum distance starting at city startpt, visiting all cities in path,
+        Minimum distance starting at city start_point, visiting all cities in path,
         and finishing at city loc
         """
         if (loc, path) not in lookuptable:
 
             if len(path) == 1 and loc in path:
-                lookuptable[(loc, path)] = rectilinear(startpt, loc)
+                lookuptable[(loc, path)] = rectilinear(start_point, loc)
             else:
                 # Enumerate all routes to this point
                 newpath = path - loc
@@ -133,11 +124,37 @@ def held_karp(startpt, cities):
 
     return lookuptable
 
-# used to estimate cost of going from a to b, must optimistic (less than the true cost)
-def heuristic(a, b):
-    (x_1, y_1) = a
-    (x_2, y_2) = b
-    return math.sqrt(((x_1 - x_2) ** 2) + ((y_1 - y_2) ** 2))
+# heuristic for estimating cost of going from a node to goal state via taking the smallest distance and multiplying by number of cities left to visit
+def naive_euclidean(graph, next):
+    # find all nodes that have been traversed
+    if next[0] != 'sink':
+        traversed = next[1] + (next[0])
+    # at goal state, cost is 0
+    else:
+        return 0
+    # find all remaining points still to be explored
+    remainder = graph.cities - set(traversed)
+    pairs = list(itertools.combinations(remainder, 2))
+    # print("===PAIRS===", pairs)
+    distances = [euclidean(p1,p2) for p1,p2 in pairs]
+    
+    return min(distances) * len(remainder)
+
+# heuristic for estimating cost of going from a node to goal state via taking the average of the remaining n smallest distances, where n = # of cities left to visit
+def avg_remaining(graph, next):
+    # find all nodes that have been traversed
+    if next[0] != 'sink':
+        traversed = next[1] + (next[0])
+    # at goal state, cost is 0
+    else:
+        return 0
+    # find all remaining points still to be explored
+    remainder = graph.cities - set(traversed)
+    pairs = list(itertools.combinations(remainder, 2))
+    distances = [euclidean(p1,p2) for p1,p2 in pairs]
+    distances.sort()
+
+    return distances[:len(remainder)] / len(remainder)
 
 # implemented using a priority queue (pop node from frontier, add neighbors of node to frontier, repeat)
 def a_star_search(graph, start, goal):
@@ -163,7 +180,7 @@ def a_star_search(graph, start, goal):
             if next not in cost_so_far or new_cost < cost_so_far[next]:
                 cost_so_far[next] = new_cost
                 # nodes with lower from_cost + heuristic_cost have higher priority (lower number)
-                priority = new_cost # + heuristic(goal, next)
+                priority = new_cost + avg_remaining(graph, next)
                 frontier.put(next, priority)
                 came_from[next] = current
     
@@ -171,6 +188,9 @@ def a_star_search(graph, start, goal):
 
 
 def main(args):
+    # FUNCTION_MAP = {'naive_euclidean' : my_top20_func,
+    #                 'avg_remaining' : my_listapps_func }
+
     parser = argparse.ArgumentParser()
     # parser.add_argument('--heuristic', nargs='+', required=True, type=str)
     parser.add_argument('--problem', required=True, type=str)
@@ -192,8 +212,16 @@ def main(args):
     start = cities[0]
     # memo = held_karp(start, cities)
     a_star_graph = Graph(start, cities)
-    # print(a_star_graph)
-    print(a_star_search(a_star_graph, ("source", None), "sink"))
+    print("HELD-KARP GRAPH:")
+    pprint.pprint(a_star_graph.__dict__)
+    print()
+
+    came_from, cost_so_far = a_star_search(a_star_graph, ("source", None), "sink")
+    print("FINAL PATH:")
+    pprint.pprint(came_from)
+    print()
+    print("TOTAL COST:")
+    pprint.pprint(cost_so_far)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
